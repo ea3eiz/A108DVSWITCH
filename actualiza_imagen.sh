d12227c0c68aec783649163e115f9b0b9529bc8c
Manel <ea3eiz@ea3eiz.com>
2022-11-15 19:11:57 +0100

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -2,9 +2,9 @@
 
                         cd /home/pi
                         sudo rm -R A108
-                        git clone http://github.com/ea3eiz/A108DVSWITCH
+                        git clone http://github.com/ea3eiz/A108NEW
                         sleep 1
-                        mv A108DVSWITCH A108
+                        mv A108NEW A108
                         sleep 1
                         sudo chmod 777 -R A108
                         cd /home/pi/A108
