91a98b2c14342a7d79cb7b6d9950224d14e18dab
Manel <ea3eiz@ea3eiz.com>
2022-11-15 12:41:02 +0100

    1

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo killall ircddbgatewayd
-sudo killall ircddbgateway
+
 mode=`grep -n -m 1 "^UARTPort=" /home/pi/MMDVMHost/MMDVMFUSION.ini`
 buscar=":"
 caracteres=`expr index $mode $buscar`
