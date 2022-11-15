461dee1a91096d661b17fcfe1521a267870ccb87
Manel <ea3eiz@ea3eiz.com>
2022-11-15 17:35:13 +0100

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -88,14 +88,14 @@ sudo systemctl stop ircddbgateway.service
 # Ejecuta Solo D-STAR 
 cd /home/pi/MMDVMHost
 #/home/pi/A108/./qt_info_solodstar & sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
-xterm -geometry 88x17+22+665 -bg black -fg green -fa ‘verdana’ -fs 9x -T CONSOLA_ESPECIAL -e sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
+xterm -geometry 88x17+1274+665 -bg black -fg green -fa ‘verdana’ -fs 9x -T CONSOLA_ESPECIAL -e sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
 
 
 
 # Cierra el icono Abrir Solo Dstar si no hay conexión  
 cd /home/pi/Desktop
 sudo cp Abrir_solodstar.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=88x19 -e sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR.png" /home/pi/Abrir_solodstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo DSTAR" /home/pi/Abrir_solodstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/pi/status.ini
@@ -179,11 +179,11 @@ sudo systemctl stop ircddbgateway.service
 # Ejecuta Solo D-STAR
 cd /home/pi/MMDVMHost
 #/home/pi/A108/./qt_info_solodstar & sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
-xterm -geometry 88x17+22+665 -bg black -fg green -fa ‘verdana’ -fs 9x -T CONSOLA_ESPECIAL -e sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
+xterm -geometry 88x17+1274+665 -bg black -fg green -fa ‘verdana’ -fs 9x -T CONSOLA_ESPECIAL -e sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
 # Cierra el icono Abrir Solo Dstar si no hay conexión 
 cd /home/pi/Desktop
 sudo cp Abrir_solodstar.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=88x19 -e sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_solodstar.sh'" /home/pi/Abrir_solodstar.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_D-STAR.png" /home/pi/Abrir_solodstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo DSTAR" /home/pi/Abrir_solodstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/pi/status.ini
