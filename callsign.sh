c2e107460a778c16d98e35481cd74389ba8b36e1
Manel <ea3eiz@ea3eiz.com>
2022-11-21 13:23:16 +0100

    1

--- callsign.sh
+++ callsign.sh
@@ -9,5 +9,5 @@ var=`expr substr $var 5 6`
 
 sudo sed -i "1c $var" /home/pi/MMDVMHost/HOLA.txt
 
-xterm -geometry 80x3+652+134 -bg black -fg white -fa ‘verdana’ -fs 19x -T ACTUALIZANDO_IMAGEN -e tail -f /home/pi/MMDVMHost/HOLA.txt
+xterm -geometry 7x1+652+134 -bg black -fg white -fa ‘verdana’ -fs 19x -T ACTUALIZANDO_IMAGEN -e tail -f /home/pi/MMDVMHost/HOLA.txt
 
