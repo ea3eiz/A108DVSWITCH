07130047995d10a75fb28006a5021f5e0fc692a9
Manel <ea3eiz@ea3eiz.com>
2022-11-21 13:19:11 +0100

    1

--- callsign.sh
+++ callsign.sh
@@ -3,18 +3,9 @@ cd /home/pi/MMDVMHost
 #var=(tail -1 MMDVM-2022-11-21.log > /home/pi/MMDVMHost/INDICATIVO.txt)
 var=$(tail -1 MMDVM-2022-11-21.log)
 
-#variable="Esto es una cadena de texto con espacios en blanco"
-#sinespacios=$(echo "$variable" | tr -d '[[:space:]]')
-#echo $sinespacios
 var=$(echo "$var" | tr -d '[[:space:]]')
-echo "variable var = $var"
-read a
-#var=$(awk "NR==1" /home/pi/MMDVMHost/INDICATIVO.txt)
-
-
 
 var=`expr substr $var 5 6`
-
 
 sudo sed -i "1c $var" /home/pi/MMDVMHost/HOLA.txt
 
