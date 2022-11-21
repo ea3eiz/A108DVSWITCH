8bd0ffc8f50fa1d0f7de4bdfb5c2458a3316b9ec
Manel <ea3eiz@ea3eiz.com>
2022-11-21 16:20:06 +0100

    1

--- callsign.sh
+++ callsign.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/pi/MMDVMHost
-#var=(tail -1 MMDVM-2022-11-21.log > /home/pi/MMDVMHost/INDICATIVO.txt)
+
 var=$(tail -1 MMDVM-2022-11-21.log)
 
 var=$(echo "$var" | tr -d '[[:space:]]')
@@ -9,5 +9,5 @@ var=`expr substr $var 70 6`
 
 sudo sed -i "1c $var" /home/pi/MMDVMHost/HOLA.txt
 
-xterm -geometry 7x2+652+134 -bg black -fg white -fa ‘verdana’ -fs 19x -T ACTUALIZANDO_IMAGEN -e tail -f /home/pi/MMDVMHost/HOLA.txt
+xterm -geometry 7x+652+134 -bg black -fg white -fa ‘verdana’ -fs 19x -T ACTUALIZANDO_IMAGEN -e tail -f /home/pi/MMDVMHost/HOLA.txt
 
