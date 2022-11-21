aa86f5929f9c4c048c610bdd0fc4277ba856cdc5
Manel <ea3eiz@ea3eiz.com>
2022-11-21 12:18:28 +0100

    1

--- callsign.sh
+++ callsign.sh
@@ -3,12 +3,8 @@ cd /home/pi/MMDVMHost
 tail -1 MMDVM-2022-11-21.log > /home/pi/MMDVMHost/INDICATIVO.txt
 
 var=$(awk "NR==1" /home/pi/MMDVMHost/INDICATIVO.txt)
-
-puerto=`expr substr $var 65 6`
-sed -i "1c $puerto" /home/pi/MMDVMHost/HOLA.txt
-
-
-
+var=`expr substr $var 65 6`
+sed -i "1c $var" /home/pi/MMDVMHost/HOLA.txt
 
 xterm -geometry 76x11+652+134 -bg black -fg white -fa ‘verdana’ -fs 19x -T ACTUALIZANDO_IMAGEN -e tail -f INDICATIVO.txt
 
