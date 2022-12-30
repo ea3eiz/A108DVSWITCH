58b35b6c4003704ff1709fd9d895bc3daca55e7f
Manel <ea3eiz@ea3eiz.com>
2022-12-30 18:57:18 +0100

    1

--- callsign.sh
+++ callsign.sh
@@ -3,19 +3,19 @@
 sudo pkill -f qt_Callsign
 cd /home/pi/MMDVMHost
 
-var=$(tail -1 MMDVM-2022-12-30.log)
+var=$(tail -2 MMDVM-2022-12-30.log)
 
 var=$(echo "$var" | tr -d '[[:space:]]')
 
-var2=`expr substr $var 70 6`
-var1=`expr substr $var 66 4`
+var2=`expr substr $var 38 16`
+var1=`expr substr $var 1 1`
 
 
 
 
 echo "var1 = $var1"
 echo "var2 = $var2"
-if [ "$var1" = "from" ];then
+if [ "$var1" = "D" ];then
 
 sudo sed -i "1c $var2" /home/pi/MMDVMHost/HOLA.txt
 #xterm -geometry 7x2+652+134 -bg black -fg white -fa ‘verdana’ -fs 19x -T ACTUALIZANDO_IMAGEN -e tail -f /home/pi/MMDVMHost/HOLA.txt
