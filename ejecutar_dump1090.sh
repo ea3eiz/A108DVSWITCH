ec83622fb3d68d1147f6e819751f193c1afc6676
Manel <ea3eiz@ea3eiz.com>
2023-01-12 20:53:10 +0100

    1

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -15,11 +15,12 @@ cd /home/pi/dump1090
 stick=$(awk "NR==40" /home/pi/status.ini)
 puerto=$(awk "NR==42" /home/pi/status.ini)
 ppm=$(awk "NR==44" /home/pi/status.ini)
+http=$(awk "NR==46" /home/pi/status.ini)
 
 if [ "$stick" = 'RSP1' ];then
-xterm -geometry 88x17+22+0 -bg brown -fg white -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo ./dump1090 --enable-agc --net --interactive --dev-sdrplay --net-ro-port $puerto --ppm $ppm 
+xterm -geometry 88x17+22+0 -bg brown -fg white -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo ./dump1090 --enable-agc --net --interactive --dev-sdrplay --net-ro-port $puerto --ppm $ppm --net-http-port $http
 else
-xterm -geometry 88x17+22+0 -bg brown -fg white -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo ./dump1090 --enable-agc --net --interactive --net-ro-port $puerto --ppm $ppm
+xterm -geometry 88x17+22+0 -bg brown -fg white -fa ‘verdana’ -fs 9x -T DUMP1090 -e sudo ./dump1090 --enable-agc --net --interactive --net-ro-port $puerto --ppm $ppm --net-http-port $http
 fi
 
 
